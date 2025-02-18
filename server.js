2d81d030c90c4bd6194a5d2bc8711bb66d70a7a7
chirayu.coder <chirayu.code@gmail.com>
2025-02-19 01:27:27 +0530

    server update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.get('/', (req, res) => {
     res.send('Hello, ES6 with Node.js!');
+});
+
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello, Chirayu from API!');
 });
 
 app.listen(PORT, () => {
